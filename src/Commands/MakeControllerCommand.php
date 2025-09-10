fcba76869a0d7e5e1f42e0c5d769ec268583d05a
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-10 13:45:53 +0700

    KDN-PC-01: Update

--- src/Commands/MakeControllerCommand.php
+++ src/Commands/MakeControllerCommand.php
@@ -17,7 +17,7 @@ class MakeControllerCommand extends Command {
 	protected function configure(): void {
 		$this
 			->setName('make:controller')
-			->setDescription('Create a new controller.                  | Eg: bin/console make:controller MyController')
+			->setDescription('Create a new controller.                  | Eg: bin/wpsp make:controller MyController')
 			->setHelp('This command allows you to create a controller.')
 			->addArgument('name', InputArgument::OPTIONAL, 'The name of the controller.');
 	}
