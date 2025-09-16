0da069130176299d8372693890dd0a670ac5a9ec
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-16 13:26:02 +0700

    KDN-PC-01: Update

--- src/Commands/MigrationMigrateCommand.php
+++ src/Commands/MigrationMigrateCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use WPSPCORE\Traits\CommandsTrait;
+use WPSPCORE\Console\Traits\CommandsTrait;
 
 class MigrationMigrateCommand extends Command {
 
