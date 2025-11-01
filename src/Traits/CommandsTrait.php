bfd3d0cbdd812b24c38a300011db7a3290fbf0b5
khanh <khanhpkvn@gmail.com>
2025-11-02 00:39:33 +0700

    KDN-PC-02: Update

--- src/Traits/CommandsTrait.php
+++ src/Traits/CommandsTrait.php
@@ -10,6 +10,10 @@ use WPSPCORE\Funcs;
 
 /**
  * @property Funcs $funcs
+ * @property \Symfony\Component\HttpFoundation\Request|\WPSPCORE\Validation\RequestWithValidation $request
+ * @property \WPSPCORE\Database\Eloquent $eloquent
+ * @property \WPSPCORE\Validation\Validation $validation
+ * @property \WPSPCORE\Environment\Environment $evironment
  */
 trait CommandsTrait {
 
@@ -18,15 +22,29 @@ trait CommandsTrait {
 	public $prefixEnv     = null;
 
 	public $funcs         = null;
-	public $coreNamespace = 'WPSPCORE';
-	public $mapRoutes     = null;
+	public $request       = null;
+	public $eloquent      = null;
+	public $validation    = null;
+	public $environment   = null;
 
-	public function __construct($name = null, $mainPath = null, $rootNamespace = null, $prefixEnv = null) {
-		parent::__construct($name);
+	public $mapRoutes     = null;
+	public $coreNamespace = 'WPSPCORE';
+
+	public $extraParams   = [];
+
+	/**
+	 * $extraParams = ['funcs', 'application', 'environment']
+	 */
+	public function __construct($mainPath = null, $rootNamespace = null, $prefixEnv = null, $extraParams = []) {
+		parent::__construct();
 		$this->mainPath      = $mainPath;
 		$this->rootNamespace = $rootNamespace;
 		$this->prefixEnv     = $prefixEnv;
-		$this->funcs         = new Funcs($this->mainPath, $this->rootNamespace, $this->prefixEnv);
+
+		$this->funcs         = $extraParams['funcs'] ?? null;
+		$this->eloquent      = $extraParams['eloquent'] ?? null;
+		$this->mapRoutes     = $extraParams['mapRoutes'] ?? null;
+		$this->environment   = $extraParams['environment'] ?? null;
 		$this->customProperties();
 	}
 
@@ -77,7 +95,7 @@ trait CommandsTrait {
 	 */
 
 	public function getRouteContent($routeName) {
-		return FileSystem::get($this->mainPath . '/routes/'.$routeName.'.php');
+		return FileSystem::get($this->mainPath . '/routes/' . $routeName . '.php');
 	}
 
 	/*
@@ -85,7 +103,7 @@ trait CommandsTrait {
 	 */
 
 	public function saveRouteContent($routeName, $content): void {
-		FileSystem::put($this->mainPath . '/routes/'.$routeName.'.php', $content);
+		FileSystem::put($this->mainPath . '/routes/' . $routeName . '.php', $content);
 	}
 
 	/*
@@ -96,7 +114,7 @@ trait CommandsTrait {
 		$routeContent = $this->getRouteContent($routeName);
 		$routeContent = preg_replace('/public function ' . $findFunction . '([\S\s]*?)\{/iu', 'public function ' . $findFunction . "$1{\n" . $newLineForFindFunction, $routeContent);
 		if (!strpos($routeContent, $newLineUseClass) !== false) {
-			$routeContent = preg_replace('/(\n\s*)class '.$routeName.' extends/iu', "\n" . $newLineUseClass . '$1class '.$routeName.' extends', $routeContent);
+			$routeContent = preg_replace('/(\n\s*)class ' . $routeName . ' extends/iu', "\n" . $newLineUseClass . '$1class ' . $routeName . ' extends', $routeContent);
 		}
 		$this->saveRouteContent($routeName, $routeContent);
 	}
